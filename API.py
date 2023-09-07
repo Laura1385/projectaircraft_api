ba21ec442e8d965caed9caa622f9b2380c404beb
Laura1385 <lauraproto13@gmail.com>
2023-09-07 12:34:30 +0200

    Completed API.py

--- API.py
+++ API.py
@@ -1,11 +1,16 @@
 #pip install fastapi
 #pip install uvicorn
+
 #uvicorn API:app --reload (modalità sviluppo)
 #uvicorn app:app  (modalità x distribuzione/normale)
 
+#http://127.0.0.1:8000/redoc
+#http://127.0.0.1:8000/docs#/
 
-from fastapi import FastAPI
+
 import json
+from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -13,9 +18,9 @@ app = FastAPI()
 with open("df_airfleets_clean.json", "r") as file:
     dati = json.load(file)
 
-#1Ok - http://127.0.0.1:8000/airfleets
-@app.get("/airfleets")
-async def get_airfleets():
+#1____ - http://127.0.0.1:8000/airlines_db
+@app.get("/airlines_db")
+async def get_airlines_db():
     #initialise a list to store data rows side by side
     result = []
 
@@ -29,12 +34,13 @@ async def get_airfleets():
 
     #Join strings using semicolons as separators
     result_string = "; ".join(result)
+    return {"Airlines Data Base": result_string}
 
-    return result_string
 
 
-#2Ok - http://127.0.0.1:8000/airline/Air%20France'
-@app.get("/airline/{airline_name}")
+
+#2____ http://127.0.0.1:8000/airline_info/Airfrance
+@app.get("/airline_info/{airline_name}")
 async def get_airline_info(airline_name: str):
     try:
         #removes spaces from the airline name
@@ -51,120 +57,39 @@ async def get_airline_info(airline_name: str):
         return {'error': f"Airline '{airline_name}' not found"}
     except KeyError as e:
         return {'error': f"Missing field: {e}"}
-
-
-#3 - 
-
-from pydantic import BaseModel
-from typing import List
-
-@app.get("/get_airline_categories", response_model=AirlineCategories)
-def get_airline_categories(country: str):
+    
+    
+    
+    
+#3____ http://127.0.0.1:8000/get_airlines_by_country/Italy   
+@app.get("/airlines_info_by_country/{country_name}")
+async def get_airlines_info_by_country(country_name: str):
     try:
-        # Inizializza i contatori per le tre categorie
-        aircraft_count = 0
-        inactive_with_aircraft_count = 0
-        inactive_merged_count = 0
+        #transform country name into uppercase
+        country_name = country_name.capitalize()
 
-        # Ciclo for per cercare il paese
-        for index, row in dati['AIRLINE'].iterrows():
-            if row['COUNTRY'].lower() == country.lower():  # Confronto ignorando maiuscole/minuscole
-                information = row['INFORMATION/N.AIRCRAFT']
+        #initialize a list to store airlines and their related data
+        compagnie_aeree = []
+        num = 0
 
-                if information.isdigit():  # Controlla se è un numero (compagnia attiva)
-                    aircraft_count += 1
-                elif information == 'inactive (with supported aircraft)':  # Compagnia scomparsa
-                    inactive_with_aircraft_count += 1
-                elif information.startswith('inactive (with supported aircraft)Renamed / Merged to'):  # Compagnia assorbita
-                    inactive_merged_count += 1
+        #loop to search for the country
+        for key, row in dati['COUNTRY'].items():
+            if row.lower() == country_name.lower():
+                airline_name = dati['AIRLINE'][key]
+                information = dati['INFORMATION/N.AIRCRAFT'][key]
+                compagnia_aerea = f"{airline_name}, {row}, {information}"
+                compagnie_aeree.append(compagnia_aerea)
+        
+        #calculate the number of found airlines
+        num = len(compagnie_aeree)
 
-        # Verifica se il paese è stato trovato
-        if aircraft_count + inactive_with_aircraft_count + inactive_merged_count > 0:
-            return AirlineCategories(country=country,
-                                     aircraft_count=aircraft_count,
-                                     inactive_with_aircraft_count=inactive_with_aircraft_count,
-                                     inactive_merged_count=inactive_merged_count)
+        #check if the country has been found
+        if compagnie_aeree:
+            #concatenate the airlines into a string separated by semicolons
+            result_string = "; ".join(compagnie_aeree)
+            return {"Airline find": num, "Airline Details": result_string}
         else:
-            return {"message": "Nessuna compagnia aerea trovata per questo paese."}
-    except Exception as e:
-        return {'error': str(e)}
-    
+            raise HTTPException(status_code=404, detail=f"Nessuna compagnia aerea trovata per il paese {country_name}")
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-'''
-
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-import json
-
-app = FastAPI()
-
-#read data from file JSON
-with open("df_airfleets_clean.json", "r") as file:
-    dati = json.load(file)
-
-#http://127.0.0.1:8000/all_airlines 
-@app.get("/all_airlines", response_class=HTMLResponse) #return all data
-async def get_airlines():
-    html_response = "<html><body><table>"
-    html_response += "<tr><th>AIRLINE</th><th>COUNTRY</th><th>INFORMATION</th></tr>"
-
-    for key, value in dati["AIRLINE"].items():
-        airline = value
-        country = dati["COUNTRY"][key]
-        information = dati["INFORMATION"][key]
-        html_response += f"<tr><td>{airline}</td><td>{country}</td><td>{information}</td></tr>"
-
-    html_response += "</table></body></html>"
-    
-    return HTMLResponse(content=html_response)
-
-
-
-@app.post("/luoghi")
-async def post_paesi(paese: str):
-    if paese in dati['luoghi']:
-        return {'dati': dati, 'message': "Località già esistente"}
-    else:
-        dati['luoghi'].append(paese)
-        return {'dati': dati, 'message': "Località aggiunta"}
-    
-@app.delete("/luoghi")
-async def delete_paesi(paese: str):
-    if paese in dati['luoghi']:
-        dati['luoghi'].remove(paese)
-        return {'data': dati, 'message':"Località eliminata"}
-    else:
-        return {'data': dati, 'message': "La località non esiste"}
-    
-'''
+    except KeyError as e:
+        raise HTTPException(status_code=500, detail=f"Missing field: {e}")
